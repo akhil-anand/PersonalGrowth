7100c11e56bb96fca2813e1fd32632df5518b6ae
akhil-anand <akhilanand.ak01@gmail.com>
2023-01-29 13:54:35 +0530

    update

--- FrontEnd/components/Dashboard/MeTime/MeTime.js
+++ FrontEnd/components/Dashboard/MeTime/MeTime.js
@@ -4,25 +4,29 @@ import React from 'react'
 import IonIcons from 'react-native-vector-icons/Ionicons'
 import Feather from 'react-native-vector-icons/Feather'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 
 const MeTime = () => {
-    const { height, width } = useWindowDimensions()
+  const { height, width } = useWindowDimensions()
   return (
     <View style={styles.container}>
-      <Text style={{marginLeft:30, marginRight:'auto', marginBottom: 20, fontSize: 18, fontStyle:'italic'}}>MeTime</Text>
-      <View style={{...styles.MeTimeItems, width, minHeight: 100}}>
-      <View style={{ flexDirection: 'row', justifyContent: "center", marginRight: 20 }}>
-        <IonIcons name='game-controller-outline' style={{ color: 'red', fontSize: 20 }} />
-        <Text>Hobbies</Text>
+      <View style={{ flexDirection: 'row', justifyContent: 'flex-start' }}>
+        <MaterialIcons name='airplanemode-inactive' style={{ color: 'black', fontSize: 23 }} />
+        <Text style={{ fontSize: 18, fontStyle: 'italic', marginStart: 5 }}>MeTime</Text>
       </View>
-      <View style={{ flexDirection: 'row', justifyContent: "center", marginRight: 20 }}>
-        <Feather name='book' style={{ color: 'green', fontSize: 20 }} />
-        <Text>Reading</Text>
-      </View>
-      <View style={{ flexDirection: 'row', justifyContent: "center", marginRight: 20 }}>
-        <MaterialCommunityIcons name='weight-lifter' style={{ color: 'blue', fontSize: 20 }} />
-        <Text>Exercise</Text>
-      </View>
+      <View style={{ ...styles.MeTimeItems, width, minHeight: 100 }}>
+        <View style={{ flexDirection: 'row', justifyContent: "center", marginRight: 20 }}>
+          <IonIcons name='game-controller-outline' style={{ color: 'red', fontSize: 20 }} />
+          <Text>Hobbies</Text>
+        </View>
+        <View style={{ flexDirection: 'row', justifyContent: "center", marginRight: 20 }}>
+          <Feather name='book' style={{ color: 'green', fontSize: 20 }} />
+          <Text>Reading</Text>
+        </View>
+        <View style={{ flexDirection: 'row', justifyContent: "center", marginRight: 20 }}>
+          <MaterialCommunityIcons name='weight-lifter' style={{ color: 'blue', fontSize: 20 }} />
+          <Text>Exercise</Text>
+        </View>
       </View>
     </View>
   )
@@ -31,18 +35,18 @@ const MeTime = () => {
 export default MeTime
 
 const styles = StyleSheet.create({
-    container: {
-        // flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    MeTimeItems:{
-        display: 'flex',
-        flexDirection: 'row',
-        padding: 10,
-        borderColor: 'black',
-        borderRadius: 5,
-        borderWidth:2
-    }
+  container: {
+    // flex: 1,
+    backgroundColor: '#fff',
+    // alignItems: 'center',
+    justifyContent: 'center',
+  },
+  MeTimeItems: {
+    display: 'flex',
+    flexDirection: 'row',
+    padding: 10,
+    borderColor: 'black',
+    borderRadius: 5,
+    borderWidth: 2
+  }
 })
