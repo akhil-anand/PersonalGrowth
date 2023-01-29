36aaadd6b575462b85134478d95aaf48605633a0
akhil-anand <akhilanand.ak01@gmail.com>
2023-01-30 01:22:39 +0530

    update

--- FrontEnd/components/TimeLine/TimeLine.js
+++ FrontEnd/components/TimeLine/TimeLine.js
@@ -3,14 +3,14 @@ import React, { useEffect, useRef, useState } from 'react'
 
 const TimeLine = () => {
 
-    const [totalTasks, setTotalTasks] = useState([])
+    const [totalTT, setTotalTT] = useState([])
 
     const [scheduleData, setScheduleData] = useState({
         work:{
             start: '10 am',
             end: '7 pm'
         },
-        Sleep:{
+        sleep:{
             start: '10 pm',
             end: '6 am'
         },
@@ -23,6 +23,59 @@ const TimeLine = () => {
     useEffect(() => {
         const tempData = generateSequentialHrs()
         
+        let workIndex1 = tempData.findIndex(item => item[0] === scheduleData?.work?.start)
+        let workIndex2 = tempData.findIndex(item => item[1] === scheduleData?.work?.end)
+
+        let arrIndexWork;
+        let arrIndexSleep;
+
+        if(workIndex2 < workIndex1){
+            arrIndexWork = [ ...Array.from({length: tempData.length - workIndex1}, (_, index) => index + workIndex1) , ...Array.from({length: workIndex2}, (_, index) => index )].sort(function(a, b){return a - b});
+        }else{
+            arrIndexWork = Array.from({length: workIndex2 - workIndex1}, (_, index) => index + workIndex1);
+        }
+
+        tempData.splice(workIndex1, workIndex2 - workIndex1, ...tempData.slice(workIndex1, workIndex2).map(item => [...item, 'work']))
+
+        let sleepIndex1 = tempData.findIndex(item => item[0] === scheduleData?.sleep?.start)
+        let sleepIndex2 = tempData.findIndex(item => item[1] === scheduleData?.sleep?.end)
+        // tempData.splice(sleepIndex1, sleepIndex2 - sleepIndex1, ...tempData.slice(sleepIndex1, sleepIndex2).map(item => [...item, 'sleep']))
+
+        if(sleepIndex2 < sleepIndex1){
+            arrIndexSleep = [ ...Array.from({length: tempData.length - sleepIndex1}, (_, index) => index + sleepIndex1) , ...Array.from({length: sleepIndex2}, (_, index) => index )].sort(function(a, b){return a - b});
+        }else{
+            arrIndexSleep = Array.from({length: sleepIndex2 - sleepIndex1}, (_, index) => index + sleepIndex1);
+        }
+
+
+
+        setTotalTT(tempData.map((item, index) => {
+            if(arrIndexWork.includes(index)){
+                return [
+                    ...item,
+                    'work',
+                    index
+                ]
+            }else if(arrIndexSleep.includes(index)){
+                return [
+                    ...item,
+                    'sleep',
+                    index
+                ]
+            }else if(index > workIndex2 && index < sleepIndex1){
+                return [
+                    ...item,
+                    'home',
+                    index
+                ]
+            }else{
+                return [
+                    ...item,
+                    'home1',
+                    index
+                ]
+            }
+        }))
 
     }, [])
     
@@ -75,13 +128,36 @@ const TimeLine = () => {
     return (
         <View style={styles.container}>
             {/* <Text>TimeLine</Text> */}
-
-            <View>
+            <ScrollView style={{flexDirection:'column'}}>
+            <View style={{backgroundColor: 'red', borderRadius: 10, paddingTop: 10}}>
+                <Text>Work Time</Text>
                 <FlatList
-                    data={generateSequentialHrs()}
+                    data={[ ...totalTT.filter((item, index, arr) => item[2] === 'work' && item[3] < arr[0]?.[3] ) ,...totalTT.filter((item, index, arr) => item[2] === 'work' && item[3] > arr[0]?.[3] )]}
                     renderItem={({ item, index }) => getHrsItem(item, index)}
                 />
             </View>
+            <View style={{backgroundColor: 'purple', borderRadius: 10, paddingTop: 10}}>
+            <Text>Home Time</Text>
+                <FlatList
+                    data={totalTT.filter(item => item[2] === 'home' )}
+                    renderItem={({ item, index }) => getHrsItem(item, index)}
+                />
+            </View>
+            <View style={{backgroundColor: 'green', borderRadius: 10, paddingTop: 10}}>
+            <Text>Sleep Time</Text>
+                <FlatList
+                    data={totalTT.filter(item => item[2] === 'sleep' )}
+                    renderItem={({ item, index }) => getHrsItem(item, index)}
+                />
+            </View>
+            <View style={{backgroundColor: 'yellow', borderRadius: 10, paddingTop: 10}}>
+            <Text>Home Time 2</Text>
+                <FlatList
+                    data={totalTT.filter(item => item[2] === 'home1' )}
+                    renderItem={({ item, index }) => getHrsItem(item, index)}
+                />
+            </View>
+            </ScrollView>
             <ScrollView>
                 <Text>Sample</Text>
                 {getTasksUnderHrs()}
