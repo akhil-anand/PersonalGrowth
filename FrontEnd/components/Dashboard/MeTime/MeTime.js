8134b5b72a6c698d685c40a7353c56844d100ef9
akhil-anand <akhilanand.ak01@gmail.com>
2023-01-29 09:48:27 +0530

    MeTime

---
+++ FrontEnd/components/Dashboard/MeTime/MeTime.js
@@ -0,0 +1,48 @@
+import { StyleSheet, Text, useWindowDimensions, View } from 'react-native'
+import React from 'react'
+
+import IonIcons from 'react-native-vector-icons/Ionicons'
+import Feather from 'react-native-vector-icons/Feather'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+
+const MeTime = () => {
+    const { height, width } = useWindowDimensions()
+  return (
+    <View style={styles.container}>
+      <Text>MeTime</Text>
+      <View style={{...styles.MeTimeItems, width}}>
+      <View style={{ flexDirection: 'row', justifyContent: "center", marginRight: 20 }}>
+        <IonIcons name='game-controller-outline' style={{ color: 'red', fontSize: 20 }} />
+        <Text>Hobbies</Text>
+      </View>
+      <View style={{ flexDirection: 'row', justifyContent: "center", marginRight: 20 }}>
+        <Feather name='book' style={{ color: 'green', fontSize: 20 }} />
+        <Text>Reading</Text>
+      </View>
+      <View style={{ flexDirection: 'row', justifyContent: "center", marginRight: 20 }}>
+        <MaterialCommunityIcons name='weight-lifter' style={{ color: 'blue', fontSize: 20 }} />
+        <Text>Exercise</Text>
+      </View>
+      </View>
+    </View>
+  )
+}
+
+export default MeTime
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    MeTimeItems:{
+        display: 'flex',
+        flexDirection: 'row',
+        padding: 10,
+        borderColor: 'black',
+        borderRadius: 5,
+        borderWidth:2
+    }
+})
---
+++ FrontEnd/components/Dashboard/WorkSleepChartView/WorkSleepChartView.js
@@ -0,0 +1,101 @@
+import { StyleSheet, Text, View, Button, ScrollView, Dimensions, useWindowDimensions } from 'react-native'
+import React, { useState } from 'react'
+import { BarChart } from 'react-native-chart-kit'
+
+const WorkSleepChartView = () => {
+
+    const { height, width } = useWindowDimensions()
+    const [chartData, setChartData] = useState({
+        workTime: {
+                labels: ["Estimated", "Actual"],
+                datasets: [
+                  {
+                    data: [11, 12]
+                  }
+                ]
+            
+        },
+        sleepTime: {
+                labels: ["Estimated", "Actual"],
+                datasets: [
+                  {
+                    data: [8, 5]
+                  }
+                ]
+            
+        },
+    })
+
+    const chartConfig = {
+        backgroundGradientFrom: "#fff",
+        backgroundGradientFromOpacity: 0,
+        backgroundGradientTo: "#fff",
+        backgroundGradientToOpacity: 0,
+        color: (opacity = 1) => `rgba(71, 71, 226, ${opacity})`,
+        strokeWidth: 1, // optional, default 3
+        barPercentage: 0.5,
+        useShadowColorFromDataset: false // optional
+      };
+
+    return (
+        <View>
+            <View style={{ flexDirection: 'row', justifyContent: "center" }}>
+                <Button
+                    // onPress={()=>{navigation.push('Dashboard')}}
+                    title="Dashboard"
+                    color="#841584"
+                    accessibilityLabel="Learn more about this purple button"
+                />
+                <Button
+                    // onPress={()=>{navigation.push('Timeline')}}
+                    title="Timeline"
+                    color="#841584"
+                    accessibilityLabel="Learn more about this purple button"
+                />
+            </View>
+            <View style={{ flexDirection: 'row', justifyContent: "space-around" }}>
+                <View style={{ padding: 50 }}>
+                    <BarChart
+                        style={{
+                            marginVertical: 8,
+                            borderRadius: 16,
+                            // margin:20
+                        }}
+                        data={chartData?.workTime}
+                        width={width / 2.2}
+                        height={height / 3}
+                        yAxisSuffix="hrs"
+                        chartConfig={chartConfig}
+                        verticalLabelRotation={0}
+                        showValuesOnTopOfBars={true}
+                        fromZero={true}
+                    //   verticalLabelRotation={30}
+                    />
+                    <Text>Work</Text>
+                </View>
+                <View style={{ padding: 50 }}>
+                    <BarChart
+                        style={{
+                            marginVertical: 8,
+                            borderRadius: 16,
+                            // margin:20
+                        }}
+                        data={chartData?.sleepTime}
+                        width={width / 2.2}
+                        height={height / 3}
+                        yAxisSuffix="hrs"
+                        chartConfig={chartConfig}
+                        verticalLabelRotation={0}
+                        showValuesOnTopOfBars={true}
+                        fromZero={true}
+                    />
+                    <Text>Sleep</Text>
+                </View>
+            </View>
+        </View>
+    )
+}
+
+export default WorkSleepChartView
+
+const styles = StyleSheet.create({})
